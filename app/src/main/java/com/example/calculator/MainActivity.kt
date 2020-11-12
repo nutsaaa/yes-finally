05060f041c41519a02b935dd056572edc6c0638b
nutsaaa <nutsa.vashakidze.1@btu.edu.ge>
2020-11-13 03:30:22 +0400

    yes finally

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var resultTextView: TextView
+
+    private var operand: Double = 0.0
+    private var operation: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        resultTextView = findViewById(R.id.resultTextView)
+
+        findViewById<TextView>(R.id.clearButton).setOnClickListener {
+            operand = 0.0
+            operation = ""
+            resultTextView.text = ""
+        }
+
+        findViewById<TextView>(R.id.deleteButton).setOnClickListener() {
+            val text = resultTextView.text.toString().removeSuffix(".0")
+            if (text.isNotEmpty()) {
+                resultTextView.text = text.dropLast(1).removeSuffix(".0")
+            } else if (text.isEmpty()){
+                resultTextView.text = operand.toString().removeSuffix(".0")
+            }
+        }
+
+    }
+
+    fun numberClick (view: View) {
+
+        if (view is TextView)  {
+
+            val number: String = view.text.toString()
+            var result: String = resultTextView.text.toString()
+
+            if (result == "0")
+                result = ""
+
+            resultTextView.text = result + number
+
+        }
+
+    }
+
+    fun opperationClick(view: View) {
+
+        if (view is TextView) {
+
+            if (!TextUtils.isEmpty(resultTextView.text)) {
+                operand = resultTextView.text.toString().toDouble()
+            }
+
+            resultTextView.text = ""
+
+            operation = view.text.toString()
+
+        }
+
+    }
+
+    fun equalsClick (view: View) {
+
+        val secOperandText: String = resultTextView.text.toString()
+        var secOperand: Double = 0.0
+
+        if (!TextUtils.isEmpty(secOperandText)) {
+            secOperand = secOperandText.toDouble()
+        }
+
+        when (operation) {
+            "+" -> resultTextView.text = (operand + secOperand).toString()
+            "-" -> resultTextView.text = (operand - secOperand).toString()
+            "*" -> resultTextView.text = (operand * secOperand).toString()
+            "/" -> resultTextView.text = (operand / secOperand).toString()
+        }
+
+    }
+
+
+
+}
